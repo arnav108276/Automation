b8209f8ef38dc9e87f111f67a048ffbecba07771
Arnav Goel <143831459+arnav108276@users.noreply.github.com>
2024-09-15 23:55:10 +0530

    Log all deleted files and archive them

--- Script10.sh
+++ Script10.sh
@@ -2,12 +2,9 @@
 DIRECTORY="/tmp"
 LOG_FILE="/var/log/delete_log.txt"
 ARCHIVE_DIR="/var/log/deleted_files"
-
 check_deleted_files() {
-    # Find deleted files in the directory
     deleted_files=$(find "$DIRECTORY" -type f -printf "%T@ %p\n" 2>/dev/null | grep "^0" | cut -d' ' -f2-)
     if [ -n "$deleted_files" ]; then
-        # Archive each deleted file
         for file in $deleted_files; do
             tar -cvf "$ARCHIVE_DIR/deleted_files_$(date +"%Y%m%d%H%M%S").tar" "$file"
             echo "$(date +"%Y-%m-%d %T") - File deleted: $file" >> "$LOG_FILE"
@@ -16,5 +13,6 @@ check_deleted_files() {
 }
 check_deleted_files()
 cat "$LOG_FILE"
+
 
 
