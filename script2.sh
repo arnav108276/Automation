edbb036b33e86b84ef195151e6a361b5a9913f41
Arnav Goel <143831459+arnav108276@users.noreply.github.com>
2024-09-16 00:05:42 +0530

    Delete all tar files in system modified in last 2 days

--- script2.sh
+++ script2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tar -cvf new_archive1.tar.gz $(find / -name *.tar -mtime -2)
+tar -cvf new_archive1.tar $(find / -name *.tar -mtime -2)
 if [rm -f new_archive1.tar]; then
 echo "Archive deleted"
 fi
